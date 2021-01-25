98bbde0cd353e386f4a5b6f91aa513393a86f961
Music <musicm987@gmail.com>
2021-01-24 18:48:56 -0700

    learning Java

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+public class Welcome{
+  public static void main(String[] args) {
+    System.out.println("My first Java Print!");
+  }
+}
+
+// public class Review{
+//   public static void main(String[] args) {
+//     // The main method executes the tasks of the class
+//     System.out.println("My first Java program from scratch!");
+//   }
+// } 
